0942e2ae1a2818e78af616cec0659995fc7bd788
Jay Tuckey <jay.tuckey@gmail.com>
2013-10-24 14:51:25 +0930

    Added RandomDeath agent.

--- study.py
+++ study.py
@@ -55,7 +55,6 @@ class Agent:
         # Now try to kill agents that are causing errors
         for agent in self.currentCalculation:
           if agent[1] != comparray[0][1]:
-            print("Agent returned incorrect calculation")
             organiser.reportAgent(agent[0])
             
         self.currentCalculation = []
@@ -109,8 +108,7 @@ class AgentPlusone(Agent):
   """ This agent is exactly the same as the main agent, except that it
   adds 1 to the result of every computation it performs. This is common in programming,
   and is known as an off-by-one error. """
-  def performCalculation(self, message):
-    calc = message.contents
+  def returnAnswer(self, calc):
     if calc[0] == "ADD":
       value = calc[1] + calc[2] + 1
     elif calc[0] == "SUB":
@@ -119,32 +117,17 @@ class AgentPlusone(Agent):
       value = calc[1] * calc[2] + 1
     if calc[0] == "DIV":
       value = calc[1] / calc[2] + 1
-      
-    # This section is all about finding who is the current leading agent
-    #  and current second in command
-    self.findCurrentLeader()
+    return value
     
-    # And now we return the result of the calculation
-    returnMessage = Message(self.id, "RES", [value])
-    if self.isLeader:
-      organiser.passMessage(self.secondInCommandID, returnMessage)
-      self.currentCalculation.append([self.id, value])
-    elif self.isSecondInCommand:
-      organiser.passMessage(self.leaderID, returnMessage)
-      self.currentCalculation.append([self.id, value])
-    else:
-      organiser.passMessage(self.leaderID, returnMessage)
-      organiser.passMessage(self.secondInCommandID, returnMessage)
 
 class AgentRandomError(Agent):
   def returnAnswer(self, calc):
     temp = random.randint(1,10)
-    if temp > 9:
+    if temp > 8:
       addOne = random.randint(-1,1)
     else:
       addOne = 0
     
-    calc = message.contents
     if calc[0] == "ADD":
       value = calc[1] + calc[2] + addOne
     elif calc[0] == "SUB":
@@ -154,12 +137,64 @@ class AgentRandomError(Agent):
     if calc[0] == "DIV":
       value = calc[1] / calc[2] + addOne
     return value
-  
-    
-
 
 class AgentRoundingError(Agent):
-  pass
+  def returnAnswer(self, calc):
+    temp = random.randint(1,10)
+    if temp > 9:
+      addOne = random.randint(-1,1)
+    else:
+      addOne = 0
+    
+    if calc[0] == "ADD":
+      value = calc[1] + calc[2] + addOne
+    elif calc[0] == "SUB":
+      value = calc[1] - calc[2] + addOne
+    if calc[0] == "MUL":
+      value = calc[1] * calc[2] + addOne
+    if calc[0] == "DIV":
+      value = calc[1] / calc[2] + addOne
+    value = round(value, 3)
+    return value
+  
+class AgentRandomDeath(Agent):
+  def __init__(self, id):
+    self.id = id
+    self.leaderID = id
+    self.isLeader = False
+    self.secondInCommandID = id
+    self.isSecondInCommand = False
+    self.currentCalculation = []
+    self.dieAt = random.randint(10,100)
+    self.currentCycles = 0
+  
+  def performCALDONE(self, message):  
+    if self.isLeader or self.isSecondInCommand:
+        comparray = []
+        for val in self.currentCalculation:
+          added = False
+          for index in range(len(comparray)):
+            if comparray[index][1] == val[1]:
+              comparray[index][0] += 1
+              added = True
+          if not added:
+            comparray.append([1, val[1]])
+        comparray.sort(reverse=True)
+        returnMessage = Message(self.id, "CALDONE", [comparray[0][1]])
+      
+        # Now try to kill agents that are causing errors
+        for agent in self.currentCalculation:
+          if agent[1] != comparray[0][1]:
+            organiser.reportAgent(agent[0])
+            
+        self.currentCalculation = []
+        
+        
+        organiser.returnResult(returnMessage)
+    if self.currentCycles == self.dieAt:
+      organiser.killAgent(self.id)
+    else:
+      self.currentCycles += 1
 
 class Organiser:
   def __init__(self, numAgents=5, argHeuristic=False):
@@ -189,8 +224,12 @@ class Organiser:
     # running
     print("Adding new agent to list")
     randomnum = random.randint(1, 100)
-    if randomnum <= 80:
+    if randomnum <= 70:
       agent = Agent(self.currentid)
+    elif randomnum <= 80:
+      agent = AgentRandomError(self.currentid)
+    elif randomnum <= 90:
+      agent = AgentRoundingError(self.currentid)
     elif randomnum <= 100:
       agent = AgentPlusone(self.currentid)
     self.currentid += 1
@@ -278,26 +317,28 @@ class Organiser:
     
   
 ##### End Classes
+organiser = False
+def run():
+  global organiser
+  organiser = Organiser(5, "INSTAKILL")
 
-organiser = Organiser(5, "INSTAKILL")
+  correctCounter = 0
+  incorrectCounter = 0
+  noReturnCounter = 0
 
-correctCounter = 0
-incorrectCounter = 0
-noReturnCounter = 0
-
-for i in range(1000):
-  val = organiser.createCalcProblem("ADD", i, 3)
-  if val == (3 + i):
-    correctCounter += 1
-  else:
-    if type(val) == int:
-      incorrectCounter += 1
+  for i in range(1000):
+    val = organiser.createCalcProblem("ADD", i, 3)
+    if (3 + i) == val:
+      correctCounter += 1
     else:
-      noReturnCounter += 1
-  organiser.killRandomAgent()
-print("Correct: " + str(correctCounter))
-print("Incorrect: " + str(incorrectCounter))
-print("No Return: " + str(noReturnCounter))
+      if type(val) == int:
+        incorrectCounter += 1
+      else:
+        noReturnCounter += 1
+    organiser.killRandomAgent()
+  print("Correct: " + str(correctCounter))
+  print("Incorrect: " + str(incorrectCounter))
+  print("No Return: " + str(noReturnCounter))
 
 
 code.interact(local=globals())
